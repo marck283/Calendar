d8cafb04a805240b51cd891e64cca850280707af
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2022-05-15 02:07:02 +0200

    Aggiunto commento per lo sviluppo dell'applicazione

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", express.static("static"));
 
 var events = require("./events.js");
-app.use("/api/v1/events", events);
+app.use("/api/v1/events", events); //Non funziona. Come mai? Forse perché eseguo la richiesta dei dati in seguito al caricamento della pagina?
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("./index.html"));
