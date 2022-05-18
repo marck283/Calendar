7b22ccab4f7ea878936f2e4df860688784ba0d33
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2022-05-18 03:16:13 +0200

    Aggiornamento path per le connessioni

--- app/app.js
+++ app/app.js
@@ -7,8 +7,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", express.static("static"));
 
 var eventList = require("./events/listaEventiPublic.js"), calendarEvents = require("./events/elencoEventiPublic.js");
-app.use("/api/v1/listaEventi", eventList);
-app.use("/api/v1/eventiCalendario", calendarEvents);
+app.use("/api/v1/EventiPubblici", eventList);
+app.use("/api/v1/GiorniCalendarioPubblico", calendarEvents);
 
 app.get("/static", (req, res) => {
     res.status(200).sendFile(path.resolve("./index.html"));
--- app/events/eventsMap.js
+++ app/events/eventsMap.js
@@ -2,7 +2,7 @@ module.exports = {
     map: function(events, routeId) {
         return events.map(event => {
             return {
-                id: '/api/v1/' + routeId + '/' + event._id,
+                id: '/api/v1/' + routeId + '/' + event._id, //Chiedere a chi di dovere se la risorsa evento debba essere ricevuta in questo modo.
                 name: event.nomeAtt,
                 category: event.categoria
             }
